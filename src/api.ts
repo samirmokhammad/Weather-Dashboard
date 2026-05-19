16653b80a1a19efc459212d0363578eaa040e90f
Samir Mokhammad <samirmokhammad13@gmail.com>
2026-05-19 19:07:26 +0200

    Fix API URLs to use HTTPS for secure requests

--- src/api.ts
+++ src/api.ts
@@ -5,31 +5,31 @@ import { AirPollutionSchema } from './schemas/airPolution';
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
-    const res = await fetch(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${API_KEY}`,
-    );
-    const data = await res.json();
-    return weatherSchema.parse(data);
+  const res = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${API_KEY}`,
+  );
+  const data = await res.json();
+  return weatherSchema.parse(data);
 }
 
 export async function getGeocode(location: string) {
-    const res = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`,
-    );
-    const data = await res.json();
-    return GeocodeSchema.parse(data);
+  const res = await fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`,
+  );
+  const data = await res.json();
+  return GeocodeSchema.parse(data);
 }
 
 export async function getAirPollution({
-    lat,
-    lon,
+  lat,
+  lon,
 }: {
-    lat: number;
-    lon: number;
+  lat: number;
+  lon: number;
 }) {
-    const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
-    );
-    const data = await res.json();
-    return AirPollutionSchema.parse(data);
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
+  );
+  const data = await res.json();
+  return AirPollutionSchema.parse(data);
 }
